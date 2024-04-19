4a6facc2c4608f921eec3458596a9a6a18dc463a
Miguel Méndez Murias <miguelmendeztech@gmail.com>
2024-04-19 10:54:47 +0200

    SDI-2324-310-SW-11.1-Servicio-SOAP-Server.

---
+++ sdi2324-310-soap-sw-server/src/main/java/com/uniovi/sdi/Sdi2324310SoapSwServerApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324310SoapSwServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2324310SoapSwServerApplication.class, args);
+	}
+
+}
